1e2f096f9562be7f34bc210307391bb8409d6bb9
Nikhil Unni <nikhil.unni@emvigotech.com>
2021-10-03 22:21:54 +0530

    single object validate

--- README.md
+++ README.md
@@ -11,15 +11,23 @@ $ npm install --save jsonash
 ```
 
 ## Usage/Examples
+
 Import jsonash into your file like below:
+
 ```javascript
-const jsonash = require('jsonash');
+const jsonash = require("jsonash");
 ```
+
 ##### 1. Sort object based on key
+
 ```javascript
-const obj = [{a: 1, b: {c: 7}}, {a: 5, b: {c: 4}}, {a: 8, b: {c: 5}}];
+const obj = [
+  { a: 1, b: { c: 7 } },
+  { a: 5, b: { c: 4 } },
+  { a: 8, b: { c: 5 } },
+];
 /* The object get sorted based on the key obj->b>c */
-const options = {desc: true, path: 'b.c'}
+const options = { desc: true, path: "b.c" };
 jsonash.sort(obj, options);
 
 /*
@@ -27,14 +35,16 @@ Output:
 [ { a: 1, b: { c: 7 } }, { a: 8, b: { c: 5 } }, { a: 5, b: { c: 4 } } ]
 */
 ```
+
 The parameter path is required that defines the depth of the object to be sorted.
 
 ##### 2. To convert all the keys to camelCase
+
 ```javascript
 const obj = {
-    first_name: "John",
-    last_name: "John",
-    phone_number: 999555
+  first_name: "John",
+  last_name: "John",
+  phone_number: 999555,
 };
 jsonash.toCamelCase(obj);
 
@@ -46,9 +56,9 @@ Output:
 /* You can pass array of objects to the method */
 
 const objArray = [
-  { first_name: 'John', last_name: 'John', phone_number: 999555 },
-  { first_name: 'Mike', last_name: 'Mike', phone_number: 123 },
-  { first_name: 'Michael', last_name: 'Michael', phone_number: 456 }
+  { first_name: "John", last_name: "John", phone_number: 999555 },
+  { first_name: "Mike", last_name: "Mike", phone_number: 123 },
+  { first_name: "Michael", last_name: "Michael", phone_number: 456 },
 ];
 jsonash.toCamelCase(objArray);
 
@@ -63,8 +73,9 @@ Output:
 ```
 
 ##### 3. To convert all the keys to snake_Case
+
 ```javascript
-const obj = { firstName: 'John', lastName: 'John', phoneNumber: 999555 };
+const obj = { firstName: "John", lastName: "John", phoneNumber: 999555 };
 jsonash.toSnakeCase(obj);
 
 /*
@@ -75,9 +86,9 @@ Output:
 /* You can pass array of objects to the method */
 
 const objArray = [
-  { firstName: 'John', lastName: 'John', phoneNumber: 999555 },
-  { firstName: 'Mike', lastName: 'Mike', phoneNumber: 123 },
-  { firstName: 'Michael', lastName: 'Michael', phoneNumber: 456 }
+  { firstName: "John", lastName: "John", phoneNumber: 999555 },
+  { firstName: "Mike", lastName: "Mike", phoneNumber: 123 },
+  { firstName: "Michael", lastName: "Michael", phoneNumber: 456 },
 ];
 jsonash.toCamelCase(objArray);
 
@@ -92,11 +103,12 @@ Output:
 ```
 
 ##### 4. To convert all the keys to PascalCase
+
 ```javascript
 const obj = {
-    first_name: "John",
-    last_name: "John",
-    phone_number: 999555
+  first_name: "John",
+  last_name: "John",
+  phone_number: 999555,
 };
 jsonash.toCamelCase(obj);
 
@@ -108,9 +120,9 @@ Output:
 /* You can pass array of objects to the method */
 
 const objArray = [
-  { first_name: 'John', last_name: 'John', phone_number: 999555 },
-  { first_name: 'Mike', last_name: 'Mike', phone_number: 123 },
-  { first_name: 'Michael', last_name: 'Michael', phone_number: 456 }
+  { first_name: "John", last_name: "John", phone_number: 999555 },
+  { first_name: "Mike", last_name: "Mike", phone_number: 123 },
+  { first_name: "Michael", last_name: "Michael", phone_number: 456 },
 ];
 jsonash.toCamelCase(objArray);
 
@@ -125,13 +137,14 @@ Output:
 ```
 
 ##### 4. To replace a key from the object
+
 ```javascript
 const obj = {
-    first_name: "John",
-    last_name: "John",
-    phone_number: 999555
+  first_name: "John",
+  last_name: "John",
+  phone_number: 999555,
 };
-jsonash.replacetKeys(obj, 'phone_number', 'mobile_number');
+jsonash.replacetKeys(obj, "phone_number", "mobile_number");
 
 /*
 Output: 
@@ -141,10 +154,10 @@ Output:
 /* You can pass array of objects or and nested objects to the method */
 
 const objArray = [
-  { first_name: 'John', last_name: 'John', phone_number: 999555 },
-  { first_name: 'Mike', last_name: 'Mike', phone_number: 123 },
-  { first_name: 'Michael', last_name: 'Michael', phone_number: 456 },
-  {user : { first_name: 'Alice', last_name: 'Alice', phone_number: 789 }}
+  { first_name: "John", last_name: "John", phone_number: 999555 },
+  { first_name: "Mike", last_name: "Mike", phone_number: 123 },
+  { first_name: "Michael", last_name: "Michael", phone_number: 456 },
+  { user: { first_name: "Alice", last_name: "Alice", phone_number: 789 } },
 ];
 jsonash.toCamelCase(objArray);
 
@@ -161,3 +174,37 @@ Output:
 */
 ```
 
+##### 4. To validate a JSON object
+
+```javascript
+const obj = { last_name: '12345', phone_number: 999555, is_admin: 'true' };
+const options = {
+    rules: {
+        first_name: {
+            type: "string",
+            max: 5,
+            min: 2,
+            required: true
+        },
+        last_name: {
+            type: "string",
+            max: 4,
+            min: 3
+        },
+        is_admin: {
+            type: 'boolean'
+        }
+    }
+}
+If the JSON is valid return true or get a response object with error messages corresponding to each field.
+/*
+Output:
+{
+  first_name: [ 'required' ],
+  last_name: [ 'last_name must contain a maximum of 4 characters.' ],
+  is_admin: [ 'is_admin must be of type boolean' ],
+  error: true
+}
+*/
+
+```
--- src/index.js
+++ src/index.js
@@ -1,83 +1,137 @@
-const { camelCase, toUpper } = require('lodash');
-const lodash = require('lodash');
-lodash.pascalCase = str => camelCase(str).replace(/^(.)/, toUpper);
+const { camelCase, toUpper } = require("lodash");
+const lodash = require("lodash");
+lodash.pascalCase = (str) => camelCase(str).replace(/^(.)/, toUpper);
 
 const getKeys = (obj) => Object.keys(obj);
 
 const getValueByIndex = (obj, is, value) => {
-  if (typeof is == 'string')
-    return getValueByIndex(obj, is.split('.'), value);
-  else if (is.length == 1 && value !== undefined)
-    return obj[is[0]] = value;
-  else if (is.length == 0)
-    return obj;
-  else
-    return getValueByIndex(obj[is[0]], is.slice(1), value);
-}
+  if (typeof is == "string") return getValueByIndex(obj, is.split("."), value);
+  else if (is.length == 1 && value !== undefined) return (obj[is[0]] = value);
+  else if (is.length == 0) return obj;
+  else return getValueByIndex(obj[is[0]], is.slice(1), value);
+};
 
 const sort = (objArray, options = {}) => {
   const desc = !!options.desc;
   if (!Array.isArray(objArray))
-    throw new Error('Argument must be of type array');
-  if (!options.path)
-    throw new Error('path is required');
+    throw new Error("Argument must be of type array");
+  if (!options.path) throw new Error("path is required");
   objArray.sort((obj1, obj2) => {
-    const a = getValueByIndex(obj1, options.path), b = getValueByIndex(obj2, options.path);
+    const a = getValueByIndex(obj1, options.path),
+      b = getValueByIndex(obj2, options.path);
     if (a < b) return desc ? 1 : -1;
     if (a > b) return desc ? -1 : 1;
     return 0;
-  })
+  });
   return objArray;
-}
+};
 
 const convertKeys = (obj, toWhich) => {
   const obj1 = {};
-  Object.keys(obj).map(item => {
-    if (typeof obj[item] === 'object' && obj[item] !== null) {
+  Object.keys(obj).map((item) => {
+    if (typeof obj[item] === "object" && obj[item] !== null) {
       obj1[lodash[`${toWhich}`](item)] = convertKeys(obj[item], toWhich);
-    }
-    else {
+    } else {
       obj1[lodash[`${toWhich}`](item)] = obj[item];
     }
-  })
+  });
   return obj1;
-}
+};
 
 const toCamelCase = (obj) => {
-  return Array.isArray(obj) ? obj.map(item => convertKeys(item, 'camelCase')) : convertKeys(obj, 'camelCase');
-}
-
+  return Array.isArray(obj)
+    ? obj.map((item) => convertKeys(item, "camelCase"))
+    : convertKeys(obj, "camelCase");
+};
 
 const toSnakeCase = (obj) => {
-  return Array.isArray(obj) ? obj.map(item => convertKeys(item, 'snakeCase')) : convertKeys(obj, 'snakeCase');
-}
+  return Array.isArray(obj)
+    ? obj.map((item) => convertKeys(item, "snakeCase"))
+    : convertKeys(obj, "snakeCase");
+};
 
 const toPascalCase = (obj) => {
-  return Array.isArray(obj) ? obj.map(item => convertKeys(item, 'pascalCase')) : convertKeys(obj, 'pascalCase');
-}
+  return Array.isArray(obj)
+    ? obj.map((item) => convertKeys(item, "pascalCase"))
+    : convertKeys(obj, "pascalCase");
+};
 
 const replacetKeysFromObj = (obj, oldKey, newKey) => {
   const obj1 = {};
-  Object.keys(obj).map(item => {
+  Object.keys(obj).map((item) => {
     let tempData;
-    if (typeof obj[item] === 'object' && obj[item] !== null) {
+    if (typeof obj[item] === "object" && obj[item] !== null) {
       tempData = replacetKeysFromObj(obj[item], oldKey, newKey);
-    }
-    else {
+    } else {
       tempData = obj[item];
     }
 
-    if (item === oldKey)
-      obj1[newKey] = tempData;
-    else
-      obj1[item] = tempData;
-  })
+    if (item === oldKey) obj1[newKey] = tempData;
+    else obj1[item] = tempData;
+  });
   return obj1;
-}
+};
 
 const replacetKeys = (obj, oldKey, newKey) => {
-  return Array.isArray(obj) ? obj.map(item => replacetKeysFromObj(item, oldKey, newKey)) : replacetKeysFromObj(obj, oldKey, newKey);
-}
+  return Array.isArray(obj)
+    ? obj.map((item) => replacetKeysFromObj(item, oldKey, newKey))
+    : replacetKeysFromObj(obj, oldKey, newKey);
+};
 
+const validateObj = (obj, options) => {
+  if (!options || !options.rules) throw new Error("No rules provided");
+  const { rules } = options;
+  const objKeys = Object.keys(rules);
+  const err = { error: false };
+  objKeys.forEach((element) => {
+    if (!!obj[element]) {
+      err[element] = [];
+      if (rules[element].type && typeof obj[element] !== rules[element].type) {
+        err[element].push(`${element} must be of type ${rules[element].type}`);
+        err.error = true;
+      } else if (
+        rules[element].type &&
+        ["string", "number"].includes(rules[element].type)
+      ) {
+        if (
+          rules[element].max &&
+          (obj[element] + "").length > rules[element].max
+        ) {
+          err[element].push(
+            `${element} must contain a maximum of ${rules[element].max} characters.`
+          );
+          err.error = true;
+        }
+        if (
+          rules[element].min &&
+          (obj[element] + "").length < rules[element].min
+        ) {
+          err[element].push(
+            `${element} must contain atleast ${rules[element].min} characters.`
+          );
+          err.error = true;
+        }
+      }
+    } else {
+      if (!!rules[element].required) {
+        err[element] = ["required"];
+        err.error = true;
+      }
+    }
+  });
+  return err;
+};
 
-module.exports = { sort, toCamelCase, toSnakeCase, toPascalCase, replacetKeys }
+const validate = (obj, options) => {
+  const err = validateObj(obj, options);
+  return (err.error && err) || true;
+};
+
+module.exports = {
+  sort,
+  toCamelCase,
+  toSnakeCase,
+  toPascalCase,
+  replacetKeys,
+  validate,
+};
